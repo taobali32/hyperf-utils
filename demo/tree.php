59bba111352c39b2f563dae708df18c0d4045aaf
DESKTOP-NQ9IVT6\ASUS <3196672779@qq.com>
2023-01-12 10:22:57 +0800

    树包

--- demo/tree.php
+++ demo/tree.php
@@ -4,4 +4,18 @@ ini_set('display_errors',1);
 require '../vendor/autoload.php';
 
 
+$foo = [
+    ['id' => 1, 'name' => '食物', 'parent_id' => 0],
+    ['id' => 4, 'name' => '饮料', 'parent_id' => 1],
+    ['id' => 5, 'name' => '矿泉水', 'parent_id' => 4],
+    ['id' => 6, 'name' => '食品', 'parent_id' => 1],
+];
+
+$tree = (new \EasyTree\Tree\TreeBuilder($foo))
+    ->setIdKey('id')
+    ->setParentKey('parent_id')
+    ->setChildrenKey('children')
+    ->build();
+
+print_r($tree->toArray());
 
